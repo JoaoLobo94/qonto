e9117cc6ff069c36d6b6fd93875b0c1a27c9a306
joao <joao.lobom@gmail.com>
2022-02-02 20:42:32 +0000

    Add async sidekiq

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,25 +4,12 @@ class Transaction < ApplicationRecord
   def self.check_transaction(transactions)
     return false unless save_balance(transactions)
 
-    bank_account = BankAccount.find_by(iban: transactions['organization_iban'],
-                                        bic: transactions['organization_bic'])
-    transactions['credit_transfers'].each do |transfer|
-      transfer_hash = {
-        counterparty_name: transfer['counterparty_name'],
-        counterparty_iban: transfer['counterparty_iban'],
-        counterparty_bic: transfer['counterparty_bic'],
-        amount_cents: transfer['amount'].to_f,
-        amount_currency: 'EUR',
-        bank_account_id: bank_account.id,
-        description: transfer['description']
-      }
-      Transaction.create(transfer_hash)
-    end
+    TransactionWorker.perform_async(transactions)
   end
 
   def self.save_balance(transactions)
     bank_account = BankAccount.find_by(iban: transactions['organization_iban'],
-                                        bic: transactions['organization_bic'])
+                                       bic: transactions['organization_bic'])
     amount_to_transact = 0
     transactions['credit_transfers'].each do |transfer|
       amount_to_transact += transfer['amount'].to_f
---
+++ app/workers/transaction_worker.rb
@@ -0,0 +1,21 @@
+class TransactionWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: true
+
+  def perform(transactions)
+    bank_account = BankAccount.find_by(iban: transactions['organization_iban'],
+                                       bic: transactions['organization_bic'])
+    transactions['credit_transfers'].each do |transfer|
+      transfer_hash = {
+        counterparty_name: transfer['counterparty_name'],
+        counterparty_iban: transfer['counterparty_iban'],
+        counterparty_bic: transfer['counterparty_bic'],
+        amount_cents: transfer['amount'].to_f,
+        amount_currency: 'EUR',
+        bank_account_id: bank_account.id,
+        description: transfer['description']
+      }
+      Transaction.create(transfer_hash)
+    end
+  end
+end
